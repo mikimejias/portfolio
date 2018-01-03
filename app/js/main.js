7820dcb3d829a187b4a95b316bef162fe5459d24
Miguel Mejias <miki.mejias85@gmail.com>
2018-01-02 21:42:46 -0400

    Primera version de mi pagina para portafolio de trabajos

---
+++ app/js/main.js
@@ -0,0 +1,12 @@
+//import { Typed } from 'typed.min.js';
+var options = {
+    strings: ["Diseño de páginas web Html5, Css3 y Javascript", "Diseño Responsive", "Landing Page", "Diseño de Logo", "Diseño de Etiquetas para productos", "Vectorización y Retoque fotográfico", "Diseño de Carátulas para libro", "Tarjetas de presentación", "Afiches, volantes - flyers", "Diseño de papelería comercial"],
+    typeSpeed: 0,
+    backSpeed: 50,
+    smartBackspace: true, // this is a default
+    backDelay:1000,
+    fadeOut: true,
+    loop: true
+};
+
+var typed = new Typed(".typed", options);
